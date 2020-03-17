78e3b6ab32471eb5a375d1e15261444e8699b789
jinalshah999 <jinalshah999@gmail.com>
2020-03-17 19:11:55 +0530

    Updates

---
+++ precache-manifest.2dda4f645d537e7bc04bfcedcd8527a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581df70632ddcfa16e9b406d7a63d05e",
+    "url": "/TodoReactApp/index.html"
+  },
+  {
+    "revision": "d6aab82ea8eda8802194",
+    "url": "/TodoReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e360402d4788c8e2ee5c",
+    "url": "/TodoReactApp/static/js/2.339e30f9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/TodoReactApp/static/js/2.339e30f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6aab82ea8eda8802194",
+    "url": "/TodoReactApp/static/js/main.a917c324.chunk.js"
+  },
+  {
+    "revision": "9f35b0a21983184e3e31",
+    "url": "/TodoReactApp/static/js/runtime-main.feaef334.js"
+  }
+]);
